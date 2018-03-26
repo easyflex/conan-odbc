4185561be42883e46ad835aea1bc22d529b1b991
Robbert Segeren <robbert.segeren@easyflex.nl>
2018-03-26 11:04:33 +0200

    Do not call make install and copy built objects from source dir

--- conanfile.py
+++ conanfile.py
@@ -15,9 +15,6 @@ class OdbcConan(ConanFile):
     options = {'shared': [True, False], 'fPIC': [True, False]}
     default_options = 'shared=False', 'fPIC=True'
 
-    source_subfolder = 'source_subfolder'
-    install_subfolder = 'install_subfolder'
-
     def configure(self):
         del self.settings.compiler.libcxx  # Pure C
         if self.settings.compiler == 'Visual Studio':
@@ -29,8 +26,7 @@ class OdbcConan(ConanFile):
         v = self.version
         source_url = 'https://iweb.dl.sourceforge.net/project/unixodbc/unixODBC/%s/unixODBC-%s.tar.gz' % (v, v)
         tools.get(source_url)
-        extracted_dir = 'unixODBC-%s' % self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        self.source_dir = 'unixODBC-%s' % self.version
 
     def build(self):
         if self.settings.os == 'Windows':
@@ -38,30 +34,52 @@ class OdbcConan(ConanFile):
         env_build = AutoToolsBuildEnvironment(self)
         static_flag = 'no' if self.options.shared else 'yes'
         shared_flag = 'yes' if self.options.shared else 'no'
-        args = ['--enable-static=%s' % static_flag, '--enable-shared=%s' % shared_flag,
-                '--enable-ltdl-install', '--prefix=%s' % os.path.realpath(self.install_subfolder)]
-        if self.settings.compiler != 'Visual Studio' and self.options.fPIC:
+        args = ['--enable-static=%s' % static_flag, '--enable-shared=%s' % shared_flag, '--enable-ltdl-install']
+        if self.options.fPIC:
             args.append('--with-pic=yes')
-        env_build.configure(configure_dir=self.source_subfolder, args=args)
-        env_build.make(args=['-j16'])
-        env_build.make(args=['install'])
+        with tools.chdir(self.source_dir):
+            env_build.configure(args=args)
+            env_build.make(args=['-j16'])
 
     def package(self):
         if self.settings.os == 'Windows':
             return
-        inc_src = os.path.join(self.install_subfolder, 'include')
-        lib_src = os.path.join(self.install_subfolder, 'lib')
-        bin_src = os.path.join(self.install_subfolder, 'bin')
-        self.copy('LICENSE*', src=self.source_subfolder)
-        self.copy('*.h',      dst='include', src=inc_src, keep_path=False)
-        self.copy('*.dylib',  dst='lib',     src=lib_src, keep_path=False)
-        self.copy('*.so',     dst='lib',     src=lib_src, keep_path=False)
-        self.copy('*.so.*',   dst='lib',     src=lib_src, keep_path=False)
-        self.copy('*.a',      dst='lib',     src=lib_src, keep_path=False)
-        self.copy('*.la',     dst='lib',     src=lib_src, keep_path=False)
-        # Note: odbc_config is excluded because it has build directory paths compiled into it,
-        # and because Conan provides the same sort of information
-        self.copy('*',        dst='bin',     src=bin_src, keep_path=False, excludes='odbc_config')
+
+        files = {
+            '': ['LICENSE*'],
+            'include': [
+                'include/autotest.h',
+                'libltdl/ltdl.h',
+                'include/odbcinst.h',
+                'include/odbcinstext.h',
+                'include/sql.h',
+                'include/sqlext.h',
+                'include/sqlspi.h',
+                'include/sqltypes.h',
+                'include/sqlucode.h',
+                'unixodbc_conf.h',
+                'include/uodbc_extras.h',
+                'include/uodbc_stats.h',
+            ],
+            'lib': [
+                'libltdl/.libs/libltdl.*',
+                'DriverManager/.libs/libodbc.*',
+                'cur/.libs/libodbccr.*',
+                'odbcinst/.libs/libodbcinst.*',
+            ],
+            'bin': [
+                'exe/dltest',
+                'exe/isql',
+                'exe/iusql',
+                'exe/odbc_config',
+                'exe/odbcinst',
+                'exe/slencheck',
+            ]
+        }
+
+        for dst, patterns in files.items():
+            for pattern in patterns:
+                self.copy(pattern, src=self.source_dir, dst=dst, keep_path=False)
 
     def package_info(self):
         self.env_info.path.append(os.path.join(self.package_folder, 'bin'))
